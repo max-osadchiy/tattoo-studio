fc3159260beb8e76286cb0a4497562a15fd413c7
max-osadchiy <max_cyber@ukr.net>
2020-04-07 23:29:05 +0300

    Merge client

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainPage from './routes/MainPage/MainPage'
+
+const App = () => {
+  return (
+    <div>
+      <MainPage/>
+    </div>
+  )
+}
+
+export default App
