99cb93533d735a4e83ab6620b8f3b0deed606741
max-osadchiy <max_cyber@ukr.net>
2020-04-07 23:13:55 +0300

    Created MainPage(first type)

--- src/routes/MainPage/MainPage.jsx
+++ src/routes/MainPage/MainPage.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Header from '../../components/Main/Header/Header'
+import TextContainer from '../../components/Main/TextContainer/TextContainer'
+import Contacts from '../../components/Main/Contacts/Contacts'
+
+import imgOne from '../../img/Main/1img.jpg'
+import './MainPage.scss'
+
 
 const MainPage = () => {
     return (
-        <div>Main Page</div>
+        <div>
+            <Header />
+            <figure>
+                <img className="img-one" src={imgOne} alt="img"/>
+            </figure>
+            <span className='high-number'>1</span>
+            <TextContainer />
+            <Contacts />
+        </div>
     )
 }
 
