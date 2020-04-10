f54a128745812f1d151176df7ada87d2d02dc2ad
max-osadchiy <max_cyber@ukr.net>
2020-04-10 23:20:58 +0300

    Created four pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import MainPage from './routes/MainPage/MainPage';
-import AboutPage from './routes/AboutPage/AboutPage';
-import ProfilePage from './routes/ProfilePage/ProfilePage';
-import menu from './components/Menu/Menu';
-import AppointmentPage from './routes/AppointmentPage/AppointmentPage';
-import { ArtistsProvider } from './contexts/ArtistsContext';
-import TattooPage from './routes/TattooPage/TattooPage';
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import MainPage from './routes/MainPage/MainPage'
+import AboutPage from './routes/AboutPage/AboutPage'
+import ProfilePage from './routes/ProfilePage/ProfilePage'
+import menu from './components/Menu/Menu'
+import AppointmentPage from './routes/AppointmentPage/AppointmentPage'
+import { ArtistsProvider } from './contexts/ArtistsContext'
+import TattooPage from './routes/TattooPage/TattooPage'
+import PiercingPage from './routes/PiercingPage/PiercingPage'
+import TattooRemovalPage from './routes/TattooRemovalPage/TattooRemovalPage'
+import PermanentMakeupPage from './routes/PermanentMakeupPage/PermanentMakeupPage'
 
 const App = () => {
   return (
@@ -14,15 +17,18 @@ const App = () => {
       <Switch>
         <Route path="/" component={MainPage} exact />
         <Route path="/appointment" component={AppointmentPage} exact />
+        <Route path="/menu" component={menu} exact />
+        <Route path="/tattoo" component={TattooPage} exact />
+        <Route path="/piercing" component={PiercingPage} exact />
+        <Route path="/tattoo-removal" component={TattooRemovalPage} exact />
+        <Route path="/permanent-makeup" component={PermanentMakeupPage} exact />
         <ArtistsProvider>
           <Route path="/about" component={AboutPage} exact />
           <Route path="/profile" component={ProfilePage} exact />
-          <Route path="/menu" component={menu} exact />
-          <Route path="/tattoo" component={TattooPage} exact />
         </ArtistsProvider>
       </Switch>
     </Router>
   );
 };
 
-export default App;
+export default App
