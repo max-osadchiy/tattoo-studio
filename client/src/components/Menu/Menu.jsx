11c406f2a6f993af747d4c4bb03e1aaceada0dac
Mishawaka <you@example.com>
2020-04-14 23:01:19 +0300

    Added menu component

--- client/src/components/Menu/Menu.jsx
+++ client/src/components/Menu/Menu.jsx
@@ -1,56 +1,168 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-import './Menu.scss'
-import cross from '../../img/cross.svg'
-import line from '../../img/LineMenu.svg'
+import './Menu.scss';
+import cross from '../../img/cross.svg';
+import line from '../../img/LineMenu.svg';
+import arrDown from '../../img/arr-down.svg';
+import arrUp from '../../img/arr-up.svg';
 
-const Menu = () => {
+const Menu = ({ show, setShow }) => {
+  // const show_service = (id) => {
+  //     if ("show_service") document.getElementById("show_service").className = 'dis-n'
+  //     document.getElementById("show_service").className = 'dis-b'
+  //     return id
+  // }
 
-    // const show_service = (id) => {
-    //     if ("show_service") document.getElementById("show_service").className = 'dis-n'
-    //     document.getElementById("show_service").className = 'dis-b'
-    //     return id
-    // }
+  // const show_artist = () => {
+  // }
 
-    // const show_artist = () => {
-
-    // }
-
-    return (
-        <div className="menu-component">
-            <img src={cross} alt=""/>
-            <div className="menu-container">
-                <div>
-                    <h1> Home</h1>
-                    <h1>About Us</h1>
-                    <h1>Services</h1>
-                    <span>
-                        <ul className="dis-n">
-                            <li><h2>Tattoo</h2></li>
-                            <li><h2>Piercing</h2></li>
-                            <li><h2>Tatto removal</h2></li>
-                            <li><h2>Permanent makeup</h2></li>
-                        </ul>
-                    </span>
-                    <h1>Tattoo Artists</h1>
-                    <span>
-                            <ul className="dis-n">
-                                <li><h2>Tattoo</h2></li>
-                                <li><h2>Piercing</h2></li>
-                                <li><h2>Tatto removal</h2></li>
-                                <li><h2>Permanent makeup</h2></li>
-                            </ul>
-                        </span>
-                    <h1>FAQ</h1>
-                </div>
-                <div>
-                    <h3>Street Tattoo Studio <br/> 16A Grecheskaya street, Odessa, Ukraine</h3>
-                    <h3>+38(096)454 36 40</h3>
-                    <h3>strtattoo@gmail.com</h3>
-                </div>
-            </div>
-        </div>
+  const [showFirst, setShowFirst] = useState(
+    new RegExp(/(tattoo|piercing|tattoo-removal|permanent-makeup)/).test(
+      window.location.pathname
     )
-}
+  );
+  const [showSecond, setShowSecond] = useState(
+    new RegExp('/about/').test(window.location.pathname)
+  );
 
-export default Menu
+  return (
+    <div
+      style={{ display: show ? 'block' : 'none' }}
+      className="menu-component"
+    >
+      <img onClick={() => setShow(!show)} src={cross} alt="" />
+      <div className="menu-container">
+        <div>
+          <div
+            className={
+              window.location.pathname === '/'
+                ? 'menu-item selected'
+                : 'menu-item'
+            }
+          >
+            <Link onClick={() => setShow(!show)} to="/">
+              <h1>Home</h1>
+            </Link>
+          </div>
+          <div
+            className={
+              window.location.pathname === '/about'
+                ? 'menu-item selected'
+                : 'menu-item'
+            }
+          >
+            <Link onClick={() => setShow(!show)} to="/about">
+              <h1>About Us</h1>
+            </Link>
+          </div>
+          <div
+            className={
+              new RegExp(
+                /(tattoo|piercing|tattoo-removal|permanent-makeup)/
+              ).test(window.location.pathname)
+                ? 'menu-item selected'
+                : 'menu-item'
+            }
+            onClick={() => setShowFirst(!showFirst)}
+          >
+            <h1>Services</h1>
+            <img
+              style={{ display: showFirst ? 'none' : 'block' }}
+              src={arrDown}
+              alt="arrow-down"
+            />
+            <img
+              style={{ display: showFirst ? 'block' : 'none' }}
+              src={arrUp}
+              alt="arrow-up"
+            />
+          </div>
+          <span style={{ display: showFirst ? 'block' : 'none' }}>
+            <ul>
+              <Link onClick={() => setShow(!show)} to="/tattoo">
+                <li>
+                  <h2>Tattoo</h2>
+                </li>
+              </Link>
+              <Link onClick={() => setShow(!show)} to="/piercing">
+                <li>
+                  <h2>Piercing</h2>
+                </li>
+              </Link>
+              <Link onClick={() => setShow(!show)} to="/tattoo-removal">
+                <li>
+                  <h2>Tatto removal</h2>
+                </li>
+              </Link>
+              <Link onClick={() => setShow(!show)} to="/permanent-makeup">
+                <li>
+                  <h2>Permanent makeup</h2>
+                </li>
+              </Link>
+            </ul>
+          </span>
+          <div
+            className={
+              new RegExp('/about/').test(window.location.pathname)
+                ? 'menu-item selected'
+                : 'menu-item'
+            }
+            onClick={() => setShowSecond(!showSecond)}
+          >
+            <h1>Tattoo Artists</h1>
+            <img
+              style={{ display: showSecond ? 'none' : 'block' }}
+              src={arrDown}
+              alt="arrow-down"
+            />
+            <img
+              style={{ display: showSecond ? 'block' : 'none' }}
+              src={arrUp}
+              alt="arrow-up"
+            />
+          </div>
+          <span style={{ display: showSecond ? 'block' : 'none' }}>
+            <ul>
+              <Link onClick={() => setShow(!show)} to="/about/andrey">
+                <li>
+                  <h2>Andrey</h2>
+                </li>
+              </Link>
+              <Link onClick={() => setShow(!show)} to="/about/oleg">
+                <li>
+                  <h2>Oleg</h2>
+                </li>
+              </Link>
+              <Link onClick={() => setShow(!show)} to="/about/anastasia">
+                <li>
+                  <h2>Nastya</h2>
+                </li>
+              </Link>
+              <Link onClick={() => setShow(!show)} to="/about/maria">
+                <li>
+                  <h2>Maria</h2>
+                </li>
+              </Link>
+            </ul>
+          </span>
+
+          <div className="menu-item">
+            <Link onClick={() => setShow(!show)} to="/about">
+              <h1>FAQ</h1>
+            </Link>
+          </div>
+        </div>
+        <div>
+          <h3>
+            Street Tattoo Studio <br /> 16A Grecheskaya street, Odessa, Ukraine
+          </h3>
+          <h3>+38(096)454 36 40</h3>
+          <h3>strtattoo@gmail.com</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
--- client/src/routes/AboutPage/AboutPage.jsx
+++ client/src/routes/AboutPage/AboutPage.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Header from '../../components/Header/Header';
 
 const AboutPage = () => {
-  return <div>Hello World(About)!!!</div>;
+  return (
+    <div>
+      <Header />
+      <h1>Hello World(About)!!!</h1>
+    </div>
+  );
 };
 
 export default AboutPage;
